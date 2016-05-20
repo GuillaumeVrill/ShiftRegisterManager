94677ad73133ca9084a753725ae9b20224eb5a84
Guillaume Vrilliaux <vrilliaux.guillaume@hotmail.com>
2016-05-20 21:57:58 +0200

    création d un fichier c++ de test utilisant la classe créée, et corection de la classe de base

--- ShiftRegisterManagerTest.cpp
+++ ShiftRegisterManagerTest.cpp
@@ -83,3 +83,31 @@ class ShiftRegisterManager{
 	}
 	
 };
+
+int main(void){
+	
+	ShiftRegisterManager *srm = new ShiftRegisterManager(8, 7, 6, 5);
+	
+	
+	
+	int *b = new int[8];
+	b[0] = 1;
+	b[1] = 1;
+	b[2] = 0;
+	b[3] = 0;
+	b[4] = 1;
+	b[5] = 1;
+	b[6] = 1;
+	b[7] = 1;
+	srm->setBus(b, 8);
+	srm->writeRegister();
+	delay(2000);
+	for(int i=0; i<8; i++){
+		b[i] = 0;
+	}
+	srm->setBus(b, 8);
+	srm->writeRegister();
+	
+	return 0;
+	
+}
